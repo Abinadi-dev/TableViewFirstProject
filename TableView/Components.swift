a113b536fb41e7c9302bd3ccfceecd6fe7f6ac66
Ondrej N <cauliflower123@seznam.cz>
2020-09-03 13:03:12 +0100

    first commit

---
+++ TableView/Components.swift
@@ -0,0 +1,9 @@
+//
+//  Components.swift
+//  TableView
+//
+//  Created by The App Experts on 03/09/2020.
+//  Copyright © 2020 The App Experts. All rights reserved.
+//
+
+import Foundation
---
+++ TableView/UIComponentViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UIComponentViewController.swift
+//  TableView
+//
+//  Created by The App Experts on 03/09/2020.
+//  Copyright © 2020 The App Experts. All rights reserved.
+//
+
+import UIKit
+
+class UIComponentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
